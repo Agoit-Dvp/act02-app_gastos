6d2fb5d6fc3ae6a36689768cc9a1467ee8792084
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-03-03 03:38:54 +0100

    MainACtiviy: Ejemplos de usos para la base de datos dbhelper

--- app/src/main/java/com/example/controlgastos/MainActivity.kt
+++ app/src/main/java/com/example/controlgastos/MainActivity.kt
@@ -11,5 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        //Ejemplo de uso de DBHelper para manipular la base de datos SQLite
+        //Creamos un objeto de tipo DBHelper
+        val dbHelper = DBHelper(this)
+
+        //Insertamos un usuario
+        val usuarioId = dbHelper.userInsert("Tiago","pjesus.tiagob92@linkifp.online","112344","03/03/2025").toInt()
+
+        //Insertar un gasto para el usuario
+        dbHelper.gastosInsert("Pizza",usuarioId, "03.03.2025", "Pizza dominos", 30.00,"pendiente", false, "mensual")
+
+        //Obtener usuarios
+        val usuarios = dbHelper.selectUsuarios()
+        usuarios.forEach{println("ID: ${it.first}, Nombre: ${it.second}")}
+
+        //Obtener gastos  del usuario
+        val gastos = dbHelper.gastosByUser(usuarioId)
+        gastos.forEach{println("ID: ${it.first}, Nombre: ${it.second}, userID: ${it.third}")}
     }
 }
