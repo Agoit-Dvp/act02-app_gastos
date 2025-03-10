5476d80c4147505b1c2b7aa1bfb418004b40d77c
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-03-10 03:06:03 +0100

    Imports sin uso

--- app/src/main/java/com/example/controlgastos/DetalleIngresoActivity.kt
+++ app/src/main/java/com/example/controlgastos/DetalleIngresoActivity.kt
@@ -9,10 +9,7 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class DetalleIngresoActivity : AppCompatActivity() {
     private lateinit var dbHelper: DBHelper
--- app/src/main/java/com/example/controlgastos/IngresoActivity.kt
+++ app/src/main/java/com/example/controlgastos/IngresoActivity.kt
@@ -5,14 +5,11 @@ import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ListView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 import java.util.Locale
 
 class IngresoActivity : AppCompatActivity() {
