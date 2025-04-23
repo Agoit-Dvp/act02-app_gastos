c12498d16c03be936184745b16a849b8c7d30ee1
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-04-23 21:32:25 +0200

    fetch: componente ForgotPassword

--- app/src/main/java/com/example/controlgastos/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/controlgastos/ui/login/LoginScreen.kt
@@ -2,6 +2,7 @@ package com.example.controlgastos.ui.login
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -17,9 +19,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.controlgastos.R
 import com.example.controlgastos.ui.theme.ControlGastosTheme
 
@@ -45,8 +49,30 @@ fun Login(modifier: Modifier) {
         EmailField()
         Spacer(modifier = Modifier.padding(4.dp))
         PasswordField()
+        Spacer(modifier = Modifier.padding(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+        Spacer(modifier = Modifier.padding(16.dp))
+        LoginButton()
     }
 
+}
+
+@Composable
+fun LoginButton() {
+    Button(onClick = {}, modifier = Modifier){
+
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        "¿Olvidaste la contraseña?",
+        modifier = modifier.clickable { },
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF483CBB),
+    )
 }
 
 @Composable
