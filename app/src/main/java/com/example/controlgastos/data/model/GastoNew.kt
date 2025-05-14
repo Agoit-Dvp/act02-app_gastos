7cf1705d6bbc1e2282b05cb6d9da7973935bda83
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-15 01:20:02 +0200

    fix: data class modelos

--- app/src/main/java/com/example/controlgastos/data/model/GastoNew.kt
+++ app/src/main/java/com/example/controlgastos/data/model/GastoNew.kt
@@ -1,10 +1,18 @@
 package com.example.controlgastos.data.model
 
+import java.util.Date
+
 data class Gasto(
     val id: String = "",
-    val cantidad: Double = 0.0,
-    val categoria: String = "",
-    val fecha: Long = System.currentTimeMillis(),
-    val descripcion: String = "",
-    val usuarioId: String = ""
+    val nombre: String = "",               // equivalente al campo nombre del gasto
+    val fecha: Date = Date(), // como timestamp
+    val valor: Double = 0.0,
+    val moneda: String? = null,
+    val categoriaId: String = "",          // antes solo 'categoria', ahora el ID (más flexible)
+    val metodoPago: String = "",
+    val estado: String = "",               // por ejemplo: "Pagado", "Pendiente"
+    val notas: String? = null,
+    val recurrente: Boolean = false,
+    val frecuencia: String? = null,        // "Mensual", "Semanal", etc.
+    val usuarioId: String = ""             // ID del usuario que hizo el gasto
 )
--- app/src/main/java/com/example/controlgastos/data/model/IngresoNew.kt
+++ app/src/main/java/com/example/controlgastos/data/model/IngresoNew.kt
@@ -1,10 +1,15 @@
 package com.example.controlgastos.data.model
 
+import java.util.Date
+
 data class Ingreso(
     val id: String = "",
-    val cantidad: Double = 0.0,
+    val nombre: String = "",
+    val valor: Double = 0.0,
     val categoria: String = "",
-    val fecha: Long = System.currentTimeMillis(),
+    val fecha: Date = Date(),
     val descripcion: String = "",
+    val recurrente: Boolean = false,
+    val categoriaId: String = "",
     val usuarioId: String = ""
 )
