09f791864d2630defa9f0786b3cb94d739bcfd9b
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-15 00:53:40 +0200

    fix: Warnings -> Unused imports

--- app/src/main/java/com/example/controlgastos/ui/usuario/UserListScreen.kt
+++ app/src/main/java/com/example/controlgastos/ui/usuario/UserListScreen.kt
@@ -1,6 +1,10 @@
 package com.example.controlgastos.ui.usuario
 
-import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
