5c57f7771519da34a8df4ce77ef344f8fa476fbe
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-17 13:58:49 +0200

    feat(model): Cambiar tipo fecha pa Date

--- app/src/main/java/com/example/controlgastos/data/model/PlanFinanciero.kt
+++ app/src/main/java/com/example/controlgastos/data/model/PlanFinanciero.kt
@@ -1,11 +1,11 @@
 package com.example.controlgastos.data.model
 
-data class PlanFinanciero(
-    val id: String = "",
-    val nombre: String,
-    val descripcion: String = "",
-    val fechaCreacion: Long = System.currentTimeMillis(),
-    val creadorId: String
-)
+import java.util.Date
 
-//He dejado System.currentTimeMillis para ver la diferencia de date
+data class PlanFinanciero(
+    val id: String = "",                    // ID del plan (Firestore document ID)
+    val nombre: String = "",
+    val descripcion: String = "",
+    val creadorId: String = "",             // ID del usuario creador
+    val fechaCreacion: Date = Date()
+)
