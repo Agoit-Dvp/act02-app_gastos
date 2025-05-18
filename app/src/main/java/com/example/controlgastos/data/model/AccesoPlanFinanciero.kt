5bf4c28d7c0c65defaea3bf879a284daad6fc578
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-19 01:02:36 +0200

    style: posible cambio de tabulacion, codigo no se ha modificado

--- app/src/main/java/com/example/controlgastos/data/model/AccesoPlanFinanciero.kt
+++ app/src/main/java/com/example/controlgastos/data/model/AccesoPlanFinanciero.kt
@@ -2,11 +2,11 @@ package com.example.controlgastos.data.model
 
 import java.util.Date
 
-data class AccesoPlanFinanciero(
-    val planId: String = "",
-    val usuarioId: String = "",
-    val rol: String = "lector",             // Ej.: "lector", "editor", "administrador"
-    val esPropietario: Boolean = false,     // Si el usuario creó el plan
-    val estado: String = "pendiente", // "pendiente", "aceptado", "rechazado"
-    val fechaAcceso: Date = Date()
-)
+    data class AccesoPlanFinanciero(
+        val planId: String = "",
+        val usuarioId: String = "",
+        val rol: String = "lector",             // Ej.: "lector", "editor", "administrador"
+        val esPropietario: Boolean = false,     // Si el usuario creó el plan
+        val estado: String = "pendiente", // "pendiente", "aceptado", "rechazado"
+        val fechaAcceso: Date = Date()
+    )
