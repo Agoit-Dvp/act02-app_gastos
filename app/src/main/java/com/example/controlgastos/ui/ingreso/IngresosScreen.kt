4d9b6e69afa1f1a41481dd8b9bf4cdb6bef0af3c
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-19 01:01:51 +0200

    perf: importar solo librerías necesarias

--- app/src/main/java/com/example/controlgastos/ui/ingreso/IngresosScreen.kt
+++ app/src/main/java/com/example/controlgastos/ui/ingreso/IngresosScreen.kt
@@ -2,14 +2,18 @@ package com.example.controlgastos.ui.ingreso
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -17,6 +21,10 @@ import com.example.controlgastos.data.model.Ingreso
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.controlgastos.ui.ingreso.components.AddIngresoSheet
 import com.example.controlgastos.ui.ingreso.components.EditIngresoSheet
 import com.example.controlgastos.ui.ingreso.components.IngresosContent
@@ -47,7 +55,7 @@ fun IngresosScreen(viewModel: IngresosViewModel = viewModel()) {
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = { showSheet = true},
+                onClick = { showSheet = true },
                 containerColor = MaterialTheme.colorScheme.primary
             ) {
                 Icon(Icons.Default.Add, contentDescription = "Agregar ingreso")
