7338d7e5d902b078f08c7523f3e052a83f30a760
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-24 23:59:17 +0200

    feat: se elimina ficheros antigua estructura

--- app/src/main/java/com/example/controlgastos/UsuarioLogueado.kt
+++
@@ -1,7 +0,0 @@
-package com.example.controlgastos
-
-object UsuarioLogueado {
-    var usuarioId: Int = -1  // -1 significa que no hay usuario logueado
-    var nombreUsuario: String = "User"
-    var emailUsuario: String = "user@correo.es"
-}
