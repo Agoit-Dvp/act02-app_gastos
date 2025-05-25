6cf9ad79b5a41cf91f9011427b6a19d78e43e528
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-25 03:24:36 +0200

    fix: unused imports MainActivity.kt

--- app/src/main/java/com/example/controlgastos/MainActivity.kt
+++ app/src/main/java/com/example/controlgastos/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.controlgastos.navigation.NavigationWrapper
 import com.example.controlgastos.ui.theme.ControlGastosTheme
@@ -18,9 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ControlGastosTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavigationWrapper()
-                }
+                        NavigationWrapper()
+                    }
 
             }
         }
