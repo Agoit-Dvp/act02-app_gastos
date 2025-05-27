afb435884cd6ca5cb6a467fc18b65e7697275357
Agoit-Dvp <pjesus.tiago@gmail.com>
2025-05-27 18:13:47 +0200

    feat: add calcularSaldo

--- app/src/main/java/com/example/controlgastos/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/controlgastos/ui/home/HomeViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel
 import com.example.controlgastos.data.model.PlanFinanciero
 import com.example.controlgastos.data.model.Usuario
 import com.example.controlgastos.data.repository.AuthRepository
+import com.example.controlgastos.data.repository.GastoRepository
+import com.example.controlgastos.data.repository.IngresoRepository
 import com.example.controlgastos.data.repository.PlanFinancieroRepository
 import com.example.controlgastos.data.repository.UsuarioRepository
 import com.google.firebase.auth.FirebaseAuth
@@ -13,7 +15,9 @@ import com.google.firebase.auth.FirebaseAuth
 class HomeViewModel(
     private val repository: UsuarioRepository = UsuarioRepository(),
     private val authRepository: AuthRepository = AuthRepository(),
-    private val planRepository: PlanFinancieroRepository = PlanFinancieroRepository()
+    private val planRepository: PlanFinancieroRepository = PlanFinancieroRepository(),
+    private val ingresoRepository: IngresoRepository = IngresoRepository(),
+    private val gastoRepository: GastoRepository = GastoRepository()
 ) : ViewModel() {
     //usuario
     private val _usuario = MutableLiveData<Usuario?>()
@@ -26,6 +30,10 @@ class HomeViewModel(
     private val _planSeleccionado = MutableLiveData<PlanFinanciero?>()
     val planSeleccionado: LiveData<PlanFinanciero?> = _planSeleccionado
 
+    //Estado para mostrar saldo
+    private val _saldo = MutableLiveData<Double>()
+    val saldo: LiveData<Double> = _saldo
+    //Cargar usuario
     fun cargarUsuario() {
         val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
 
@@ -37,16 +45,31 @@ class HomeViewModel(
             }
         }
     }
-
+    //Cargar plan seleccionado
     fun cargarPlanSeleccionado(planId: String) {
         planRepository.obtenerPlanPorId(planId) { plan, errorMsg ->
             if (errorMsg != null) {
                 _error.value = errorMsg
             } else {
                 _planSeleccionado.value = plan
+                calcularSaldo(planId)
             }
         }
     }
+    //Calcular saldo
+    private fun calcularSaldo(planId: String) {
+        ingresoRepository.obtenerTotalIngresos(planId) { totalIngresos ->
+            gastoRepository.obtenerTotalGastos(planId) { totalGastos ->
+                _saldo.postValue(totalIngresos - totalGastos)
+            }
+        }
+    }
+    //Funcion para volver a llamar a calcularSaldo
+    fun actualizarSaldo(planId: String) {
+        calcularSaldo(planId) // función privada ya existente
+    }
+
+
 
     fun cerrarSesion() {
         authRepository.logout()
